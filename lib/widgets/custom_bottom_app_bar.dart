2cfea80945373c104b559c3bd65c60ce0189aba5
adel_daniel <adel.daniel.222@gmail.com>
2022-01-26 15:06:14 +0200

    add user icon pressed functionality

--- lib/widgets/custom_bottom_app_bar.dart
+++ lib/widgets/custom_bottom_app_bar.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import '../screens/cart_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_ecommerce/features/auth/application/bloc/auth_bloc.dart';
+import 'package:my_ecommerce/features/auth/presentation/screens/login_screen.dart';
 
+import '../screens/cart_screen.dart';
 import 'cart_badge.dart';
 
 class CustomBottmAppBar extends StatelessWidget {
@@ -27,7 +30,16 @@ class CustomBottmAppBar extends StatelessWidget {
                 child:
                     const CartBadge()), // const Icon(Icons.shopping_cart, color: Colors.white)),
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  final authState = context.read<AuthBloc>().state;
+
+                  if (authState.isAuthenticated) {
+                    // TODO  will navigate to the user profile
+                  } else {
+                    // the user is not logging in
+                    Navigator.pushNamed(context, LoginScreen.routeName);
+                  }
+                },
                 icon: const Icon(Icons.person, color: Colors.white)),
           ],
         ),
